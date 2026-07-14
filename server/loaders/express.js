8242f04bf2bd9d786a49cfc354627d6ceafe32a4
Cserpi <cserpakdaniel2010@gmail.com>
2026-07-14 07:55:02 +0200

    Update CORS origin to match deployed Netlify domain

--- server/loaders/express.js
+++ server/loaders/express.js
@@ -9,7 +9,7 @@ const expressLoader = () => {
   app.use(
     cors({
       origin: [
-        'https://myfeed-demo.netlify.app',
+        'https://myfeed-typescript.netlify.app',
         'http://localhost:3001',
       ],
       methods: ['GET', 'POST'],
